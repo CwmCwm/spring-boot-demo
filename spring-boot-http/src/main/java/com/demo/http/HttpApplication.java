a9d56b6ed63e7110ac7326e0735dad82b454bb4f
cwm <514225516@qq.com>
2019-12-30 10:25:35 +0800

    添加spring-boot-banner和spring-boot-http

---
+++ spring-boot-http/src/main/java/com/demo/http/HttpApplication.java
@@ -0,0 +1,17 @@
+package com.demo.http;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpApplication {
+
+	/**
+	 * resources/ 目录下的banner.png就是springboot启动时控制台的输出，这里用来Tomcat的图标，你可以换图片试试
+	 * resources/ 目录下的banner.txt就是springboot启动时控制台的输出，这里是hello world，你可以改一下试试
+	 * */
+	public static void main(String[] args) {
+		SpringApplication.run(HttpApplication.class, args);
+	}
+
+}
